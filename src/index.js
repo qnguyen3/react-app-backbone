1c061e889720889025bb66956c1d621d6edbd260
Quan Nguyen <quanjenkey@gmail.com>
2022-08-18 15:39:07 +0700

    adding files and components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <h1>Hello World</h1>,
-  document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"));
